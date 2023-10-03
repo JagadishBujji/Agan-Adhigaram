23ac5533c2bd167d39bd1cec1593b17642127b8b
JagadishBujji <jagadish00198@gmail.com>
2023-10-03 19:13:18 +0530

    magical

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,12 @@ import Aboutus from "../components/Aboutus";
 import Hero from "../components/Hero";
 import Journey from "../components/Journey";
 import Magical from "../components/Magical";
-import Value from "../components/Value";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Aboutus />
-      {/* <Value/> */}
       <Magical />
       <Journey />
     </>
