a02f1a0bdc42ebb3dee1b770499776bbfa1c4a02
kabilan1234 <kabilan250r@gmail.com>
2023-10-04 12:48:36 +0530

    author

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,17 @@
 import Aboutus from "../components/Aboutus";
+import Author from "../components/Author";
 import Hero from "../components/Hero";
 import Journey from "../components/Journey";
 import Value from "../components/Value";
+import Welcome from "../components/Welcome";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Aboutus/>
+      <Welcome/>
+      <Author/>
       {/* <Value/> */}
       <Journey/>
     </>
