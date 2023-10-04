149e44d6cd7514440007868889656ba6cab0a470
JagadishBujji <jagadish00198@gmail.com>
2023-10-04 14:20:40 +0530

    svg

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Nav from "./Layout/Navbar/Navbar";
-import Hero from "./components/Hero";
-import Aboutus from "./components/Aboutus";
-import Value from "./components/Value";
+
+
 import Router from "./routes";
 function App() {
   return (
     <div>
-      {/* <Nav/>
-    <Hero/>
-    <Aboutus/>
-    <Value/> */}
       <Router />
     </div>
   );
