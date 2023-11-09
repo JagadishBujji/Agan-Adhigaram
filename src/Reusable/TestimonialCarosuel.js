2671748421ae01ccf0a97758600dcfd41bb20099
sanjay-3129 <ksanjay00197@gmail.com>
2023-11-09 13:00:44 +0530

    :sparkles: feat(ui): testimonial map

--- src/Reusable/TestimonialCarosuel.js
+++ src/Reusable/TestimonialCarosuel.js
@@ -4,6 +4,34 @@ import "./TestimonialCarosuel.css"; // Import your CSS file
 const TestimonialCarousel = () => {
   const [activeTestimonial, setActiveTestimonial] = useState(3); // Initially, the third testimonial is active (1-based index).
 
+  const testimonials = [
+    {
+      author: "Princy, Web Developer",
+      desc: "Tamil publishing startup Agan Adhigaram presents popular stories as Board books, with interactive features such as sliders and flaps designed for little hands.",
+      company: "The Hindu",
+    },
+    {
+      author: "Rian",
+      desc: "English publishing startup Agan Adhigaram presents popular stories as Board books, with interactive features such as sliders and flaps designed for little hands.",
+      company: "The English",
+    },
+    {
+      author: "Franco",
+      desc: "France publishing startup Agan Adhigaram presents popular stories as Board books, with interactive features such as sliders and flaps designed for little hands.",
+      company: "The France",
+    },
+    {
+      author: "Manoj",
+      desc: "Manoj publishing startup Agan Adhigaram presents popular stories as Board books, with interactive features such as sliders and flaps designed for little hands.",
+      company: "The Manoj",
+    },
+    {
+      author: "Kumar",
+      desc: "Kumar publishing startup Agan Adhigaram presents popular stories as Board books, with interactive features such as sliders and flaps designed for little hands.",
+      company: "The Kumar",
+    },
+  ];
+
   const handleTestimonialChange = (index) => {
     setActiveTestimonial(index);
   };
@@ -20,7 +48,7 @@ const TestimonialCarousel = () => {
 
   return (
     <div className="slider">
-      {Array.from({ length: 5 }, (_, i) => (
+      {testimonials.map((testi, i) => (
         <input
           type="radio"
           name="testimonial"
@@ -32,19 +60,16 @@ const TestimonialCarousel = () => {
       ))}
 
       <div className="testimonials">
-        {Array.from({ length: 5 }, (_, i) => (
+        {/* {Array.from({ length: 5 }, (_, i) => ( */}
+        {testimonials.map((testi, i) => (
           <label
             className={`item ${activeTestimonial === i + 1 ? "active" : ""}`}
             htmlFor={`t-${i + 1}`}
             key={`testimonial-${i}`}
           >
-            <p>
-              Tamil publishing startup Agan Adhigaram presents popular stories
-              as Board books, with interactive features such as sliders and
-              flaps designed for little hands
-            </p>
-            <h2>Princy, Web Developer</h2>
-            <h2>The Hindu</h2>
+            <p>{testi.desc}</p>
+            <h2>{testi.author}</h2>
+            <h2>{testi.company}</h2>
             {/* <img src="https://dummyimage.com/150" alt="picture" /> */}
           </label>
         ))}
