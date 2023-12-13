a24ad2640eaaa414d3cb5a23c878ff4b925f577c
JagadishBujji <jagadish00198@gmail.com>
2023-12-13 13:08:08 +0530

    :lipstick: style(ui): ui correction

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -22,11 +22,8 @@ function Welcome() {
               style={{ textAlign: "center" }}
             >
               We craft books like toys that they can play with. When a book
-              <br />
               becomes a toy, it transforms learning into a joyful experience.
-              <br />
               Children engage with the stories, flipping pages with excitement,
-              <br />
               and in doing so, they're not just reading; they're playing.
             </p>
             <span>
